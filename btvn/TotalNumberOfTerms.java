efa48404bc0e852dd0e493a09f2c92016b0fa7fe
ht2401 <89507638+ht2401@users.noreply.github.com>
2022-09-13 17:05:58 +0700

    Update TotalNumberOfTerms.java

--- btvn/TotalNumberOfTerms.java
+++ btvn/TotalNumberOfTerms.java
@@ -3,10 +3,8 @@ package btvn;
 public class TotalNumberOfTerms {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 0; i < 100; i++) {
-            if (i % 2 == 0) {
-                sum += i;
-            }
+        for (int i = 0; i < 100; i = i + 2) {
+            sum += i;
         }
         System.out.println("The sum of the numbers from 1 to 99 is : " + sum);
     }
